6b56020972542bac08f2c44032dbfb8f3dede609
1stby <love970120@gmail.com>
2024-02-26 13:25:08 +0800

    add Jinja 模板：迴圈 + 繼承

--- flask101/app.py
+++ flask101/app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 app = Flask(__name__)
 load_dotenv()
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return 'Hello World!!!'
+    user = {'username': 'Toby'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Portland 的天氣真好！'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': '復仇者聯盟電影真的很酷！'
+        }
+    ]
+    return render_template('index.html', title = '首頁', user=user, posts=posts)
 
 if __name__ == '__main__':
     app.run()
