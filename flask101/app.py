8613ffe6b91914f9c32e35132860a0cccf4b279a
1stby <love970120@gmail.com>
2024-02-26 14:55:46 +0800

    add 實作 Login 畫面+實作驗證輸入

--- flask101/app.py
+++ flask101/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
 from dotenv import load_dotenv
+from forms import LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 load_dotenv()
 
 @app.route('/')
@@ -18,6 +21,15 @@ def index():
         }
     ]
     return render_template('index.html', title = '首頁', user=user, posts=posts)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('使用者 {} 的登入請求，記住我 ={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='登入', form=form)
 
 if __name__ == '__main__':
     app.run()
