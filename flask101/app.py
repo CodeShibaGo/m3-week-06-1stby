3e2e3428465b94b9f6504f6fbad20c68e5079753
1stby <love970120@gmail.com>
2024-02-26 16:37:45 +0800

    add Flask-SQLAlchemy 連接上 MySQL

--- flask101/app.py
+++ flask101/app.py
@@ -1,10 +1,31 @@
 from flask import Flask, render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from dotenv import load_dotenv
+import os
 from forms import LoginForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
 load_dotenv()
+
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@localhost:{os.getenv('DB_HOST_PORT')}/{os.getenv('DB_NAME')}"
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    _id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column('name', db.String(100))
+    email = db.Column(db.String(100))
+    mobile = db.Column('mobile', db.String(100))
+    def __init__(self, name, email, mobile):
+        self.name = name
+        self.email = email
+        self.mobile = mobile
 
 @app.route('/')
 @app.route('/index')
@@ -20,9 +41,9 @@ def index():
             'body': '復仇者聯盟電影真的很酷！'
         }
     ]
-    return render_template('index.html', title = '首頁', user=user, posts=posts)
+    return render_template('index.html', title='首頁', user=user, posts=posts)
 
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -30,6 +51,8 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='登入', form=form)
+
+
 
 if __name__ == '__main__':
     app.run()
